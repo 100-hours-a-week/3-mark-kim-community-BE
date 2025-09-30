991a5c53baefdbfbb4c9357e3d7cc9423da62ce6
kimminjun <rlaalswns7769@gmail.com>
2025-09-30 15:21:48 +0900

    User 엔티티 수정 - 회원정보 테이블명 users로 설정

--- src/main/java/kr/adapterz/community/entity/User.java
+++ src/main/java/kr/adapterz/community/entity/User.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Entity
+@Entity(name = "users")
 @Getter @Setter
 public class User {
     @Id
---
+++ src/main/java/kr/adapterz/community/entity/UserAuth.java
@@ -0,0 +1,4 @@
+package kr.adapterz.community.entity;
+
+public class UserAuth {
+}
