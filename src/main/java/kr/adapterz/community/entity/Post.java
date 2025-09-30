ce586ab4e56903966ccd183be76f0b72a687956f
kimminjun <rlaalswns7769@gmail.com>
2025-09-30 17:23:13 +0900

    Post 엔티티 구현

---
+++ src/main/java/kr/adapterz/community/entity/Post.java
@@ -0,0 +1,31 @@
+package kr.adapterz.community.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "posts")
+@Getter @Setter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 26, nullable = false)
+    private String title;
+    @Column(columnDefinition = "LONGTEXT", nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    @ManyToOne()
+    @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.SET_NULL)
+    private User user;
+}
