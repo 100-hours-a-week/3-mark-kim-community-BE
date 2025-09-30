8d9f71062b201ba380a1941988fa3a69fde9b8ea
kimminjun <rlaalswns7769@gmail.com>
2025-09-30 17:41:41 +0900

    PostImage 엔티티 구현

---
+++ src/main/java/kr/adapterz/community/entity/PostImage.java
@@ -0,0 +1,28 @@
+package kr.adapterz.community.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "post_images")
+@Getter @Setter
+public class PostImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 320, nullable = false)
+    private String imagePath;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne()
+    @JoinColumn(name = "post_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Post post;
+}
