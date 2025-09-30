b2d0d74673d258506458d82675ced477b9eed2f6
kimminjun <rlaalswns7769@gmail.com>
2025-09-30 16:22:19 +0900

    UserAuth 엔티티 구현

--- src/main/java/kr/adapterz/community/entity/UserAuth.java
+++ src/main/java/kr/adapterz/community/entity/UserAuth.java
@@ -1,4 +1,22 @@
 package kr.adapterz.community.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity(name = "users_auth")
+@Getter @Setter
 public class UserAuth {
+    @Id
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @Column(length = 320, nullable = false)
+    private String email;
+    @Column(length = 100, nullable = false)
+    private String password;
 }
