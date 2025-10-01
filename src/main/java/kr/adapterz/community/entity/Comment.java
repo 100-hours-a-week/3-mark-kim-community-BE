1c9ef85d9041c1e283ff2a9197bda768cd2ce78e
kimminjun <rlaalswns7769@gmail.com>
2025-10-01 13:02:55 +0900

    Comment 엔티티 수정 - @Getter, @Setter 어노테이션 추가, 생성자 구현

--- src/main/java/kr/adapterz/community/entity/Comment.java
+++ src/main/java/kr/adapterz/community/entity/Comment.java
@@ -1,12 +1,15 @@
 package kr.adapterz.community.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDateTime;
 
 @Entity(name = "comments")
+@Getter @Setter
 public class Comment {
 
     @Id
@@ -30,4 +33,13 @@ public class Comment {
     @JoinColumn(name = "user_id")
     @OnDelete(action = OnDeleteAction.SET_NULL)
     private User user;
+
+    protected Comment() {}
+
+    public  Comment(String content, Post post, User user) {
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+        this.post = post;
+        this.user = user;
+    }
 }
