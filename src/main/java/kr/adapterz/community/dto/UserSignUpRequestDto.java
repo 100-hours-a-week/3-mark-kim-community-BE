1e9b2a8b298ac57ef789e5fa4c61423db8fcd917
kimminjun <rlaalswns7769@gmail.com>
2025-10-01 17:23:08 +0900

    UserDto 수정 - UserSignUpRequestDto로 이름 변경

--- src/main/java/kr/adapterz/community/dto/UserSignUpRequestDto.java
+++ src/main/java/kr/adapterz/community/dto/UserSignUpRequestDto.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class UserDto {
+public class UserSignUpRequestDto {
     private String email;
     private String password;
     private String nickname;
