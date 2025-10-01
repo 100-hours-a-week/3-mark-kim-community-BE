35527f62a9cf3b92493c6530589f972e876cbc2e
kimminjun <rlaalswns7769@gmail.com>
2025-10-01 17:28:35 +0900

    UserService 인터페이스 생성 - 회원가입 요청 작업을 처리하는 메서드 구현

---
+++ src/main/java/kr/adapterz/community/service/UserService.java
@@ -0,0 +1,15 @@
+package kr.adapterz.community.service;
+
+import kr.adapterz.community.dto.UserSignUpRequestDto;
+import kr.adapterz.community.dto.UserSignUpResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+    /*
+        회원가입 요청 시 호출
+        회원가입 요청 본문을 받아 User 엔티티를 생성
+        생성된 엔티티를 DB에 저장하고 저장된 데이터 정보를 반환
+    */
+    public UserSignUpResponseDto saveUser(UserSignUpRequestDto userSignUpRequestDto);
+}
