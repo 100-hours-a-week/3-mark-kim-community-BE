65812dc928b18d3d304b354dcc4859d9addcf363
kimminjun <rlaalswns7769@gmail.com>
2025-10-01 17:17:47 +0900

    UserDto 생성

---
+++ src/main/java/kr/adapterz/community/dto/UserDto.java
@@ -0,0 +1,12 @@
+package kr.adapterz.community.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserDto {
+    private String email;
+    private String password;
+    private String nickname;
+    private String profileImage;
+}
