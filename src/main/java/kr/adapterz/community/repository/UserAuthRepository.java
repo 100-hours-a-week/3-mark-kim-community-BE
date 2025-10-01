db5c295d18dc3adef6f33b431238ebcdc5df89cb
kimminjun <rlaalswns7769@gmail.com>
2025-10-01 17:39:00 +0900

    UserAuthRepository 생성 - UserService 구현에 필요

---
+++ src/main/java/kr/adapterz/community/repository/UserAuthRepository.java
@@ -0,0 +1,9 @@
+package kr.adapterz.community.repository;
+
+import kr.adapterz.community.entity.UserAuth;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserAuthRepository extends JpaRepository<UserAuth, Long> {
+}
