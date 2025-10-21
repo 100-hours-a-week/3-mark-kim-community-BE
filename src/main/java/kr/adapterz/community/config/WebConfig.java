67f04e4aa09447a82b5304e8ac3362cf1d3322ee
kimminjun <rlaalswns7769@gmail.com>
2025-10-21 18:10:23 +0900

    WebConfig 수정 - allowedOrigins 3000 포트로 변경

--- src/main/java/kr/adapterz/community/config/WebConfig.java
+++ src/main/java/kr/adapterz/community/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500") // 허용할 출처
+                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000") // 허용할 출처
                 .allowedMethods("*") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(86400); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
